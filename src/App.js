566432a36fc470f7eba59dcf4fa0adce76497626
Jatin Hinduja <jatinhinduja4@gmail.com>
2023-05-09 00:58:49 +0530

    2

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: "e5",
+      title: "Milk",
+      amount: 50,
+      date: new Date(2021, 5, 8),
+    },
   ];
 
   return (
