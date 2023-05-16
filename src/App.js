10799982c94e763a47f5d7bf218ffefc1537a010
Jatin Hinduja <jatinhinduja4@gmail.com>
2023-05-16 19:11:49 +0530

    filter year

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ const DUMMY_EXPENSES = [
 ];
 const App=()=> {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -9,6 +9,22 @@ const Expenses=(props)=> {
   const filterChangeHandler=(selectedYear)=>{
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses=props.items.filter(expense=>{
+    return expense.date.getFullYear().toString()===filteredYear
+  })
+
+  let expensesContent= <p>No Expenses Found</p>;
+  if(filteredExpenses.length>0){
+    expensesContent=filteredExpenses.map((expense)=>(
+      <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}/>
+    ));
+  }
+  
+
   return (
     <div>
      <Card className="expenses">
@@ -16,16 +32,9 @@ const Expenses=(props)=> {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-      {props.items.map((expense)=>(
-        <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}/>
-      ))}
-    
+        {expensesContent}
      </Card>
      </div> 
   );
 }
-
 export default Expenses;
